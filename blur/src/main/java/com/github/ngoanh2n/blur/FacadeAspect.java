54e987ebb818a1e2e51af2ee7ff4207ccf02da12
ngoanh2n <ngoanh2n@gmail.com>
2023-04-04 21:12:34 +0700

    Aspect: Register AllureSelenide to SelenideLogger

--- blur/src/main/java/com/github/ngoanh2n/blur/FacadeAspect.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/FacadeAspect.java
@@ -3,7 +3,9 @@ package com.github.ngoanh2n.blur;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.ngoanh2n.Commons;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -17,7 +19,6 @@ public class FacadeAspect {
     @Before("execution(* com.codeborne.selenide.Selenide.open(..))")
     public void selenideOpen() {
         if (!facadeIntercepted) {
-            facadeIntercepted = true;
             BlurConfig config = new BlurConfig();
             BlurDriver driver = new BlurDriver(config);
             BlurContainer container = new BlurContainer(config);
@@ -27,6 +28,9 @@ public class FacadeAspect {
             Commons.writeField(Configuration.class, "defaults", config);
             Commons.writeField(WebDriverRunner.class, "webdriverContainer", container);
             Commons.writeField(WebDriverRunner.class, "staticSelenideDriver", selenideDriver);
+
+            facadeIntercepted = true;
+            SelenideLogger.addListener("Allure", new AllureSelenide());
         }
     }
 }
