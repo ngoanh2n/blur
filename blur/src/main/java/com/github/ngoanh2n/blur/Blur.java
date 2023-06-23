c5654da3fcd5cf33611aa67fd7296449e712524c
ngoanh2n <ngoanh2n@gmail.com>
2023-06-23 21:25:21 +0700

    [javadoc] Format usage codes

--- blur/src/main/java/com/github/ngoanh2n/blur/Blur.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/Blur.java
@@ -18,65 +18,65 @@ import org.openqa.selenium.By;
  *     <li>{@link Selectors}<br>
  *          Define a locator by a locating strategy.
  *         <pre>{@code
- *             By by = Selectors.byXpath(..)
- *             By by = Selectors.withText(..)
+ *             By by = Selectors.byXpath(..);
+ *             By by = Selectors.withText(..);
  *             ...
  *         }</pre>
  *     </li>
  *     <li>{@link Selenide#$(By) Blur.$(..)}<br>
  *          Find element/elements by a locator.
  *          <pre>{@code
- *              SelenideElement element = Blur.$(..)
- *              ElementsCollection elements = Blur.$$(..)
+ *              SelenideElement element = Blur.$(..);
+ *              ElementsCollection elements = Blur.$$(..);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link Condition}<br>
  *          Define a condition to check element.
  *          <pre>{@code
- *              Condition condition = Condition.enabled
- *              Condition condition = Condition.exactText(..)
+ *              Condition condition = Condition.enabled;
+ *              Condition condition = Condition.exactText(..);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link SelenideElement}<br>
  *          Interact with element.
  *          <pre>{@code
- *              SelenideElement element = element.dragAndDrop(..)
- *              SelenideElement element = element.shouldBe(condition)
+ *              SelenideElement element = element.dragAndDrop(..);
+ *              SelenideElement element = element.shouldBe(condition);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link CollectionCondition}<br>
  *          Define a collection condition to check elements.
  *          <pre>{@code
- *              CollectionCondition condition = CollectionCondition.size(..)
- *              CollectionCondition condition = CollectionCondition.texts(..)
+ *              CollectionCondition condition = CollectionCondition.size(..);
+ *              CollectionCondition condition = CollectionCondition.texts(..);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link ElementsCollection}<br>
- *          Interact with elements.
+ *          Interact with list of elements.
  *          <pre>{@code
- *              ElementsCollection elements = elements.filter(condition)
- *              ElementsCollection elements = elements.shouldBe(condition)
+ *              ElementsCollection elements = elements.filter(condition);
+ *              ElementsCollection elements = elements.shouldBe(condition);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link WebDriverChecker}<br>
  *          Check WebDriver characteristics and environment.
  *          <pre>{@code
- *              boolean result = WebDriverChecker.isIOS(..)
- *              boolean result = WebDriverChecker.isSafari(..)
+ *              boolean result = WebDriverChecker.isIOS(..);
+ *              boolean result = WebDriverChecker.isSafari(..);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link WebDriverShooter}<br>
  *          Take screenshot with WebDriver and comparison.
  *          <pre>{@code
- *              Screenshot screenshot = WebDriverShooter.page(..)
- *              Screenshot screenshot = WebDriverShooter.frame(..)
- *              Screenshot screenshot = WebDriverShooter.element(..)
+ *              Screenshot screenshot = WebDriverShooter.page(..);
+ *              Screenshot screenshot = WebDriverShooter.frame(..);
+ *              Screenshot screenshot = WebDriverShooter.element(..);
  *          }</pre>
  *     </li>
  * </ul>
@@ -104,9 +104,9 @@ import org.openqa.selenium.By;
  *     <li>{@link Resources}<br>
  *          Find and read Java resources.
  *          <pre>{@code
- *              File file = Resources.getFile("file.json")
- *              String content = Resources.getContent("file.yml")
- *              InputStream is = Resources.getInputStream("file.png")
+ *              File file = Resources.getFile("file.json");
+ *              String content = Resources.getContent("file.yml");
+ *              InputStream is = Resources.getInputStream("file.png");
  *          }</pre>
  *     </li>
  *     <li>{@link YamlData}<br>
@@ -114,31 +114,31 @@ import org.openqa.selenium.By;
  *          <pre>{@code
  *              public class User extends YamlData {}
  *              ---
- *              User user = new User().fromResource("user.yml").toModel()
- *              List<User> users = new User().fromResource("users.yml").toModels()
+ *              User user = new User().fromResource("user.yml").toModel();
+ *              List<User> users = new User().fromResource("users.yml").toModels();
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link Property}<br>
  *          Represent a JVM system property.
  *          <pre>{@code
- *              Property property = Property.ofString(..)
- *              Property property = Property.ofBoolean(..)
+ *              Property property = Property.ofString(..);
+ *              Property property = Property.ofBoolean(..);
  *              ...
  *          }</pre>
  *     </li>
  *     <li>{@link PropertiesFile}<br>
  *          Read properties file.
  *          <pre>{@code
- *              PropertiesFile propertiesFile = new PropertiesFile("file.properties")
- *              String value = propertiesFile.getProperty("keyName")
+ *              PropertiesFile propertiesFile = new PropertiesFile("file.properties");
+ *              String value = propertiesFile.getProperty("keyName");
  *          }</pre>
  *     </li>
  *     <li>{@link AllureEnvironment}<br>
  *          Write {@code environment.properties} to Allure results directory.
  *          <pre>{@code
- *              AllureEnvironment.write(properties)
- *              AllureEnvironment.write("file1.properties", "file2.properties")
+ *              AllureEnvironment.write(properties);
+ *              AllureEnvironment.write("file1.properties", "file2.properties");
  *          }</pre>
  *     </li>
  *     <li>{@link EnabledIfProperty @EnabledIfProperty}<br>
@@ -159,7 +159,7 @@ import org.openqa.selenium.By;
  *              ---
  *              public class MyTest {
  *                  &#064;Test
- *                  &#064;SetProperty(name = "propertyName", value = "propertyValue")
+ *                  &#064;SetProperty(name = "browser", value = "safari")
  *                  public void test () {}
  *              }
  *          }</pre>
@@ -200,25 +200,37 @@ public final class Blur extends Selenide {
      * Switch to a WebDriver among the list of browsers.<br>
      * Browser list is combined via:
      * <ul>
-     *     <li>Config: <em>selenide.browser</em>. {@link BlurConfig#browser()}</li>
-     *     <li>Config: <em>selenide.otherBrowsers</em>. {@link BlurConfig#otherBrowsers()}</li>
+     *     <li>
+     *         Config <em>selenide.browser</em>
+     *         <ul>
+     *             <li>Getter: {@link BlurConfig#browser() BlurConfig#browser()}</li>
+     *             <li>Setter: {@link BlurConfig#browser(String) BlurConfig#browser(String)}</li>
+     *         </ul>
+     *     </li>
+     *     <li>
+     *         Config <em>selenide.otherBrowsers</em>
+     *         <ul>
+     *             <li>Getter: {@link BlurConfig#otherBrowsers() BlurConfig#otherBrowsers()}</li>
+     *             <li>Setter: {@link BlurConfig#otherBrowsers(String) BlurConfig#otherBrowsers(String)}</li>
+     *         </ul>
+     *     </li>
      * </ul>
-     * <p>
-     * Example:
+     *
+     * <b>Example:</b>
      * <pre>{@code
-     * System.setProperty("selenide.browser", "chrome");
-     * System.setProperty("selenide.otherBrowsers", "edge,firefox");
+     *      System.setProperty("selenide.browser", "chrome");
+     *      System.setProperty("selenide.otherBrowsers", "edge,firefox");
      *
-     * Blur.open(URL);                 // Chrome
+     *      Blur.open(URL);                 // Chrome
      *
-     * Blur.switchToWebDriver(1);      // Edge
-     * Blur.open(URL1);
+     *      Blur.switchToWebDriver(1);      // Edge
+     *      Blur.open(URL1);
      *
-     * Blur.switchToWebDriver(2);      // Firefox
-     * Blur.open(URL2);
+     *      Blur.switchToWebDriver(2);      // Firefox
+     *      Blur.open(URL2);
      *
-     * Blur.switchToWebDriver(0);      // Chrome
-     * Blur.switchToWebDriver(1);      // Edge
+     *      Blur.switchToWebDriver(0);      // Chrome
+     *      Blur.switchToWebDriver(1);      // Edge
      * }</pre>
      *
      * @param index The index in browser list (0-based indexing).
