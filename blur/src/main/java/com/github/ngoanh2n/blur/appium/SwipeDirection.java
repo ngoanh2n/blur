4c6be1a5c95005de6112a5247cb8389683ef2a89
ngoanh2n <ngoanh2n@gmail.com>
2023-06-25 23:00:31 +0700

    [javadoc] Add for SwipeDirection

--- blur/src/main/java/com/github/ngoanh2n/blur/appium/SwipeDirection.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/appium/SwipeDirection.java
@@ -1,5 +1,17 @@
 package com.github.ngoanh2n.blur.appium;
 
+/**
+ * Four directions for swiping to.<br><br>
+ *
+ * <em>Repository:</em>
+ * <ul>
+ *     <li><em>GitHub: <a href="https://github.com/ngoanh2n/blur">ngoanh2n/blur</a></em></li>
+ *     <li><em>Maven: <a href="https://mvnrepository.com/artifact/com.github.ngoanh2n/blur">com.github.ngoanh2n:blur</a></em></li>
+ * </ul>
+ *
+ * @author ngoanh2n
+ * @since 2020
+ */
 public enum SwipeDirection {
     UP, DOWN, LEFT, RIGHT
 }
