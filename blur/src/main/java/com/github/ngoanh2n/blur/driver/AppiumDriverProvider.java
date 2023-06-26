8baab5859e0db789f10e3ace6796778e024abb7e
ngoanh2n <ngoanh2n@gmail.com>
2023-06-26 22:37:38 +0700

    Implement AppiumDriverProvider

--- blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverProvider.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverProvider.java
@@ -1,4 +1,17 @@
 package com.github.ngoanh2n.blur.driver;
 
-public class AppiumDriverProvider {
+import com.codeborne.selenide.WebDriverProvider;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class AppiumDriverProvider implements WebDriverProvider {
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        return AppiumDriverService.createDriver(capabilities);
+    }
 }
