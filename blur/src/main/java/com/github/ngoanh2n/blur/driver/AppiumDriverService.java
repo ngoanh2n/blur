7a526c0eb662d6567c31a19b5b8c3764ea7640b2
ngoanh2n <ngoanh2n@gmail.com>
2023-06-26 22:34:04 +0700

    AppiumDriverService: Create driver

--- blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverService.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverService.java
@@ -2,14 +2,17 @@ package com.github.ngoanh2n.blur.driver;
 
 import com.github.ngoanh2n.Property;
 import com.github.ngoanh2n.YamlData;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Map;
 
@@ -17,6 +20,12 @@ import java.util.Map;
 public final class AppiumDriverService {
     private static final Logger log = LoggerFactory.getLogger(AppiumDriverService.class);
     private static final Property<String> capsResource = Property.ofString("blur.caps");
+
+    public static WebDriver createDriver(@Nullable Capabilities otherCaps) {
+        AppiumDriverLocalService service = startServer();
+        Capabilities caps = readCaps().merge(otherCaps);
+        return new AppiumDriver(service, caps);
+    }
 
     public static Capabilities readCaps() {
         DesiredCapabilities caps = new DesiredCapabilities();
