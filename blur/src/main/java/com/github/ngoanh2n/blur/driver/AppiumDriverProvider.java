3e23a977908e0971feee6784045cd5e454dcc7c1
ngoanh2n <ngoanh2n@gmail.com>
2023-06-29 23:10:52 +0700

    [blur] Add overloading method AppiumDriverProvider.startDriver(..)

--- blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverProvider.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverProvider.java
@@ -8,8 +8,8 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,14 +23,30 @@ public class AppiumDriverProvider implements WebDriverProvider {
     private static final Logger log = LoggerFactory.getLogger(AppiumDriverProvider.class);
     private static final Property<String> capsResource = Property.ofString("blur.caps");
 
-    public static WebDriver startDriver(@Nullable Capabilities otherCaps) {
-        AppiumDriverLocalService service = startServer();
-        Capabilities caps = readCaps().merge(otherCaps);
-        return new AppiumDriver(service, caps);
+    public static AppiumDriverLocalService startServer() {
+        AppiumServiceBuilder builder = new AppiumServiceBuilder()
+                .withIPAddress("127.0.0.1")
+                .usingAnyFreePort()
+                .withArgument(GeneralServerFlag.BASEPATH, "/wd/hub")
+                .withArgument(() -> "--allow-insecure", "chromedriver_autodownload");
+        return startServer(builder);
     }
 
-    public static Capabilities readCaps() {
-        DesiredCapabilities caps = new DesiredCapabilities();
+    public static AppiumDriverLocalService startServer(AppiumServiceBuilder builder) {
+        AppiumDriverLocalService service = AppiumDriverLocalService.buildService(builder);
+        Runtime.getRuntime().addShutdownHook(new StopAppiumServerThread(service));
+
+        if (!service.isRunning()) {
+            service.start();
+            log.debug("Appium server started");
+        } else {
+            log.debug("Appium server has started already");
+        }
+        return service;
+    }
+
+    public static Capabilities readCapabilities() {
+        MutableCapabilities caps = new MutableCapabilities();
         Map<String, Object> providedCaps = YamlData.toMapFromResource(capsResource.getValue());
         providedCaps.forEach(caps::setCapability);
 
@@ -42,22 +58,10 @@ public class AppiumDriverProvider implements WebDriverProvider {
         return caps;
     }
 
-    public static AppiumDriverLocalService startServer() {
-        AppiumServiceBuilder builder = new AppiumServiceBuilder()
-                .withIPAddress("127.0.0.1")
-                .usingAnyFreePort()
-                .withArgument(GeneralServerFlag.BASEPATH, "/wd/hub")
-                .withArgument(() -> "--allow-insecure", "chromedriver_autodownload");
-        AppiumDriverLocalService service = AppiumDriverLocalService.buildService(builder);
-        Runtime.getRuntime().addShutdownHook(new StopAppiumServerThread(service));
-
-        if (!service.isRunning()) {
-            service.start();
-            log.debug("Appium server started");
-        } else {
-            log.debug("Appium server has started already");
-        }
-        return service;
+    public static WebDriver startDriver(@Nullable Capabilities otherCaps) {
+        AppiumDriverLocalService service = startServer();
+        Capabilities caps = readCapabilities().merge(otherCaps);
+        return new AppiumDriver(service, caps);
     }
 
     //-------------------------------------------------------------------------------//
