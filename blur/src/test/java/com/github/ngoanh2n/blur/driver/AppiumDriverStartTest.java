af032d99d0b0ef37468d8f82cb3717d41cfa27ed
ngoanh2n <ngoanh2n@gmail.com>
2023-06-29 21:24:12 +0700

    [test] Create driver for Appium

--- blur/src/test/java/com/github/ngoanh2n/blur/driver/AppiumDriverStartTest.java
+++ blur/src/test/java/com/github/ngoanh2n/blur/driver/AppiumDriverStartTest.java
@@ -1,4 +1,30 @@
 package com.github.ngoanh2n.blur.driver;
 
+import com.codeborne.selenide.WebDriverRunner;
+import com.github.ngoanh2n.SetProperty;
+import com.github.ngoanh2n.blur.Blur;
+import com.github.ngoanh2n.blur.BlurConfig;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ngoanh2n
+ */
+@SetProperty(name = "blur.caps", value = "caps/android-emulator-native.yml")
+@SetProperty(name = "selenide.browser", value = "com.github.ngoanh2n.blur.driver.AppiumDriverProvider")
 public class AppiumDriverStartTest {
+    @Test
+    void openAppiumDriver() {
+        Blur.open();
+        BlurConfig config = Blur.getConfig();
+        Assertions.assertNotNull(config);
+    }
+
+    @AfterEach
+    void closeWebDriver() {
+        if (WebDriverRunner.hasWebDriverStarted()) {
+            WebDriverRunner.closeWebDriver();
+        }
+    }
 }
