44dcde56eec545c22f704c7ba7fa87713b6367b6
ngoanh2n <ngoanh2n@gmail.com>
2023-06-30 23:10:34 +0700

    [test] Add browser constants

--- blur/src/test/java/com/github/ngoanh2n/blur/Constant.java
+++ blur/src/test/java/com/github/ngoanh2n/blur/Constant.java
@@ -4,4 +4,9 @@ package com.github.ngoanh2n.blur;
  * @author ngoanh2n
  */
 public final class Constant {
+    public final static class Browser {
+        public final static String EDGE = "edge";
+        public final static String CHROME = "chrome";
+        public final static String APPIUM = "com.github.ngoanh2n.blur.driver.AppiumDriverProvider";
+    }
 }
