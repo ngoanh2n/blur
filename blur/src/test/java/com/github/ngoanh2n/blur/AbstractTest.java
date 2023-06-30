f6f4216aa5fce89f42836910bcc558fdd0e418bd
ngoanh2n <ngoanh2n@gmail.com>
2023-06-30 23:07:19 +0700

    [test] Add AfterEach method for AbstractTest

--- blur/src/test/java/com/github/ngoanh2n/blur/AbstractTest.java
+++ blur/src/test/java/com/github/ngoanh2n/blur/AbstractTest.java
@@ -1,7 +1,14 @@
 package com.github.ngoanh2n.blur;
 
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+
 /**
  * @author ngoanh2n
  */
 public abstract class AbstractTest {
+    @AfterEach
+    public void closeWebDriver() {
+        WebDriverRunner.closeWebDriver();
+    }
 }
