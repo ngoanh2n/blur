b0550a64c2ba5d3ae1dd8f6c717b8aaf0430159c
ngoanh2n <ngoanh2n@gmail.com>
2023-06-30 23:08:34 +0700

    [test] Set some configs for AbstractTest

--- blur/src/test/java/com/github/ngoanh2n/blur/AbstractTest.java
+++ blur/src/test/java/com/github/ngoanh2n/blur/AbstractTest.java
@@ -1,11 +1,18 @@
 package com.github.ngoanh2n.blur;
 
 import com.codeborne.selenide.WebDriverRunner;
+import com.github.ngoanh2n.SetProperty;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * @author ngoanh2n
  */
+@SetProperty(name = "selenide.timeout", value = "5000")
+@SetProperty(name = "selenide.remoteConnectionTimeout", value = "10000")
+@SetProperty(name = "selenide.remoteReadTimeout", value = "30000")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public abstract class AbstractTest {
     @AfterEach
     public void closeWebDriver() {
