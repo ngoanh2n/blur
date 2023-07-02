355bbf61fd965d59ee7dee6f6872aa9762440fd8
ngoanh2n <ngoanh2n@gmail.com>
2023-07-02 10:23:13 +0700

    [javadoc] Usage: AppiumDriverProvider

--- blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverProvider.java
+++ blur/src/main/java/com/github/ngoanh2n/blur/driver/AppiumDriverProvider.java
@@ -17,11 +17,80 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+/**
+ * Start a AppiumDriver with the capabilities.<br>
+ * Procedure steps:
+ * <ol>
+ *     <li>Start Appium server</li>
+ *     <li>Read Appium capabilities</li>
+ *     <li>Start Appium driver</li>
+ *     <li>Stop Appium server</li>
+ * </ol>
+ *
+ * <b>Specify capabilities</b><br>
+ * Use system property {@code blur.appium.capabilities} to specify which capabilities are used for {@link AppiumDriver}.<br><br>
+ *
+ * <b>Specify browser</b><br>
+ * Use system property {@code selenide.browser} to ask Selenide to start {@link AppiumDriver}.<br><br>
+ *
+ * <b>Example</b>
+ * <ol>
+ *     <li>Specify capabilities resource</li>
+ *     <li>Ask Selenide to start {@link AppiumDriver} via {@link #createDriver(Capabilities) AppiumDriverProvider.createDriver(capabilities)}
+ *     </li>
+ * </ol>
+ *
+ * <pre>{@code
+ *      // Representing how Capabilities is located inside the project structure
+ *      src
+ *      ├── main
+ *      └── test
+ *      	├── java
+ *      	└── resources
+ *      		├── log4j.properties
+ *      		└── android-chrome-emulator.yml
+ * }</pre>
+ * <pre>{@code
+ *      // Demo test codes
+ *      &#064;Test
+ *      public void test() {
+ *          System.setProperty("blur.appium.capabilities", "android-chrome-emulator.yml");
+ *          System.setProperty("selenide.browser", "com.github.ngoanh2n.blur.driver.AppiumDriverProvider");
+ *          Blur.open();
+ *      }
+ * }</pre><br>
+ *
+ * <em>Repository:</em>
+ * <ul>
+ *     <li><em>GitHub: <a href="https://github.com/ngoanh2n/blur">ngoanh2n/blur</a></em></li>
+ *     <li><em>Maven: <a href="https://mvnrepository.com/artifact/com.github.ngoanh2n/blur">com.github.ngoanh2n:blur</a></em></li>
+ * </ul>
+ *
+ * @author ngoanh2n
+ * @since 2020
+ */
 @ParametersAreNonnullByDefault
 public class AppiumDriverProvider implements WebDriverProvider {
     private static final Logger log = LoggerFactory.getLogger(AppiumDriverProvider.class);
     private static final Property<String> capabilitiesResource = Property.ofString("blur.appium.capabilities");
 
+    /**
+     * Start Appium driver with capabilities from Yaml file in resources folder.
+     *
+     * @param otherCapabilities The capabilities for merging to Appium capabilities from Yaml file.
+     * @return A {@link AppiumDriver}.
+     */
+    public static WebDriver startDriver(@Nullable Capabilities otherCapabilities) {
+        AppiumDriverLocalService service = startServer();
+        Capabilities capabilities = readCapabilities().merge(otherCapabilities);
+        return new AppiumDriver(service, capabilities);
+    }
+
+    /**
+     * Start Appium server and register a new virtual-machine shutdown hook for stopping server automatically.
+     *
+     * @return A {@link AppiumServiceBuilder}.
+     */
     public static AppiumDriverLocalService startServer() {
         AppiumServiceBuilder builder = new AppiumServiceBuilder()
                 .withIPAddress("127.0.0.1")
@@ -34,6 +103,12 @@ public class AppiumDriverProvider implements WebDriverProvider {
         return startServer(builder);
     }
 
+    /**
+     * Start Appium server and register a new virtual-machine shutdown hook for stopping server automatically.
+     *
+     * @param builder To set server information and arguments.
+     * @return A {@link AppiumDriverLocalService}.
+     */
     public static AppiumDriverLocalService startServer(AppiumServiceBuilder builder) {
         AppiumDriverLocalService service = AppiumDriverLocalService.buildService(builder);
         Runtime.getRuntime().addShutdownHook(new StopAppiumServerThread(service));
@@ -48,6 +123,12 @@ public class AppiumDriverProvider implements WebDriverProvider {
         return service;
     }
 
+    /**
+     * Read Appium capabilities from Yaml file in {@code resources} folder.<br>
+     * Use system property {@code blur.appium.capabilities} to specify capabilities resource.<br><br>
+     *
+     * @return A {@link Capabilities}.
+     */
     public static Capabilities readCapabilities() {
         String capabilitiesFile = capabilitiesResource.getValue();
         MutableCapabilities capabilities = new MutableCapabilities();
@@ -59,12 +140,6 @@ public class AppiumDriverProvider implements WebDriverProvider {
             }
         });
         return capabilities;
-    }
-
-    public static WebDriver startDriver(@Nullable Capabilities otherCapabilities) {
-        AppiumDriverLocalService service = startServer();
-        Capabilities capabilities = readCapabilities().merge(otherCapabilities);
-        return new AppiumDriver(service, capabilities);
     }
 
     //-------------------------------------------------------------------------------//
